a3b2ecd2b042898011c5f4130b3fb2ba0d03a32f
Tiago Garcia <tiagoromerogarcia@gmail.com>
2013-12-25 17:13:10 -0200

    introduzindo bootstrap + menu

--- eai-cms/app/controllers/ControladorDeTipoDeProduto.java
+++ eai-cms/app/controllers/ControladorDeTipoDeProduto.java
@@ -7,42 +7,54 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import repositories.RepositorioDeTipoDeProduto;
 import views.html.TipoDeProduto.listar;
-import views.html.TipoDeProduto.novo;
+import views.html.TipoDeProduto.editar;
 import views.html.TipoDeProduto.ver;
 import br.com.casadocodigo.eai.modelos.TipoDeProduto;
 
 public class ControladorDeTipoDeProduto extends Controller {
+	@play.db.jpa.Transactional
 	public static Result listar() {
 		List<TipoDeProduto> tiposDeProduto = RepositorioDeTipoDeProduto.listar();
         return ok(listar.render(tiposDeProduto));
     }
 	
 	public static Result novo() {
-        return ok(novo.render());
+		Form<TipoDeProduto> form = Form.form(TipoDeProduto.class);
+        return ok(editar.render("Criar", form));
     }
 	
+	@play.db.jpa.Transactional
 	public static Result ver(Long id) {
 		TipoDeProduto tipoDeProduto = RepositorioDeTipoDeProduto.obter(id);
         return ok(ver.render(tipoDeProduto));
     }
 	
+	@play.db.jpa.Transactional
+	public static Result editar(Long id) {
+		Form<TipoDeProduto> form = Form.form(TipoDeProduto.class);
+		form.fill(RepositorioDeTipoDeProduto.obter(id));
+        return ok(editar.render("Editar",  form));
+    }
+	
+	@play.db.jpa.Transactional
 	public static Result salvar() {
 		Form<TipoDeProduto> form = Form.form(TipoDeProduto.class);
 		TipoDeProduto tipoDeProduto = form.bindFromRequest().get();
 		try {
 			RepositorioDeTipoDeProduto.salvar(tipoDeProduto);
-			return ok();
+			return listar();
 		} catch (Exception e) {
 			return internalServerError(e.getMessage());
 		}
     }
 	
+	@play.db.jpa.Transactional
 	public static Result remover(Long id) {
 		try {
 			TipoDeProduto tipoDeProduto = RepositorioDeTipoDeProduto.obter(id);
 			if (tipoDeProduto != null) {
 				RepositorioDeTipoDeProduto.remover(tipoDeProduto);
-				return ok();
+				return listar();
 			}
 			else {
 				return notFound();
